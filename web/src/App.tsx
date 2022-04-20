837292eda5f341db5f22b9e6f788ab5d7fb1ce5a
Raskolnikovv <raskolnikovsete@gmail.com>
2022-04-19 22:21:29 -0300

    dia 2

---
+++ web/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
+import NewPage from './pages/New';
+
+function App() {
+
+  const theme = {
+    primary: "#322153",
+    secondary: "#6C63FF",
+    background: "#F0F0F5",
+    text: "#6C6C80",
+    white: "#fff"
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/new" element={<NewPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
